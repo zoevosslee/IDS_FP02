a594cb628fa0f99b6960124dbcbbc84d5e0190d7
zoevosslee <zvl@mit.edu>
2025-03-28 18:18:41 -0400

    added nav bar

---
+++ src/routes/plan/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <title>Rent is a Trap!</title>
+</svelte:head>
+
+<div id="home-page">
+  <div class="container">
+    <div class="text-content">
+      <h1>Rent is a Trap!</h1>
+      <h3>Project Plan</h3>
+      <p>this is where our project plan will go</p>
+    </div>
+  </div>
+</div>
---
+++ src/routes/writeup/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <title>Rent is a Trap!</title>
+</svelte:head>
+
+<div id="home-page">
+  <div class="container">
+    <div class="text-content">
+      <h1>Rent is a Trap!</h1>
+      <h3>Writeup</h3>
+      <p>this is where our write up will go</p>
+    </div>
+  </div>
+</div>
