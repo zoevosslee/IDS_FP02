b0798065fdc0bd2ccc0854d804d726d9f3945328
00hn03y <y30nh00@gmail.com>
2025-04-06 13:44:38 -0400

    update tooltip
    except quartile

--- src/lib/FeatureInfoPanel.svelte
+++ src/lib/FeatureInfoPanel.svelte
@@ -18,9 +18,15 @@
       <p><strong>GEOID:</strong> {feature.properties.GEOID}</p>
   
       {#if year === 2023}
-        <p><strong>Education (2023):</strong> {feature.properties.BachelorOrHigher2023}%</p>
+        <p><strong>BA or Higher (2023):</strong> {Math.ceil(feature.properties.BachelorOrHigher2023 * 100)}%</p>
+        <p><strong>Median Income (2023):</strong> ${Math.ceil(feature.properties.MedianIncome2023)}</p>
+        <p><strong>White (2023):</strong> {Math.ceil(feature.properties.White2023*100)}%</p>
+        <p><strong>Rent Burdened (2023):</strong> {Math.ceil(feature.properties.RentBurden2023*100)}%</p>
       {:else}
-        <p><strong>Education (2015):</strong> {feature.properties.BachelorOrHigher2015}%</p>
+        <p><strong>BA or Higher (2015):</strong> {Math.ceil(feature.properties.BachelorOrHigher2015*100)}%</p>
+        <p><strong>Median Income (2015):</strong> ${Math.ceil(feature.properties.MedianIncome2015)}</p>
+        <p><strong>White (2015):</strong> {Math.ceil(feature.properties.White2015*100)}%</p>
+        <p><strong>Rent Burdened (2015):</strong> {Math.ceil(feature.properties.RentBurden2015*100)}%</p>
       {/if}
   
       <p><strong>Quartile:</strong> {feature.properties.quartile}</p>
